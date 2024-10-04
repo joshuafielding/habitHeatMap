c3d0005b3a7602191d6fbe9acb82f16e0681df25
Joshua Fielding <114246326+joshuafielding@users.noreply.github.com>
2024-10-03 21:50:35 -0400

    v0.2
    added new settings and fixed bugs

--- habiticaHeatMap.js
+++ habiticaHeatMap.js
@@ -1,85 +1,149 @@
-//===================================================DAY CREATION / DELETION AND VIEW MANAGER===================================================
+//===================================================GLOBAL VARIABLES===================================================
 //gets the date
 const currentDate = new Date();
+//gets the day
+const currentDay = currentDate.getDay() - 1;
 //gets the month from the date (0-11)
 const currentMonth = currentDate.getMonth();
 //gets the year from the date
 const currentYear = currentDate.getFullYear();
 //user selected month (0-11)
-let selectedMonth = currentMonth; 
+let selectedMonth = Number(currentMonth); 
 //user selected year
 let selectedYear = currentYear;
-//initialize the view to 1
-let view = 1;
 //initialize the format for the date
-let dateFormat = "mdy";
+let dateFormat = document.querySelector('input[name="formatSettings"]:checked').value;
+//color of borders around day boxes
+let borderColor = "#000000";
+//allows us to put days in dayContainer
+const dayContainer = document.querySelector(".dayContainer");
 
+//color variables used for changing background color for days... not working as expected fix later.
+let futureColor = "#000000";
+let hundredColor = "#00ff00";
+let seventyFiveColor = "#b0c800";
+let fiftyColor = "#d79f00";
+let twentyFiveColor = "#f46900";
+let zeroColor = "#ff0000";
 
+//initialize the view to what the user previously checked (or the default value)
+let view = document.querySelector('input[name="viewSettings"]:checked').value;
+//if a user leaves the page with ytd selected, we have to do this calculation.
+//ytd variable to check selection
+let ytdSelected = false;
+//this is used for ytd
+let prevSelectedMonth;
+
+if(view == "ytd"){
+    ytdSelected = true;
+    selectedMonth = currentMonth;
+    view = currentMonth;
+    dayChanger(view, "ytd");
+}else{
+    //show current month on screen when loading in. 
+    createDays(selectedMonth, selectedYear, view);
+    addDayClickListeners();
+}
+
+//===================================================DAY CREATION / DELETION AND VIEW MANAGER===================================================
 //function to find how many days in a month
 function daysInMonth(month, year) {
     return new Date(year, month + 1, 0).getDate();
 }
 
-//allows us to put days in dayContainer
-const dayContainer = document.querySelector(".dayContainer")
-
-//show current month
-createDays(selectedMonth, selectedYear, 1);
-addDayClickListeners();
-
 // Create and append day divs inside the existing container
 function createDays(monthPassed, selectedYear, numMonths) {
     // Controls how big the boxes are
     let flex;
-    let fontsize;
-    console.log(numMonths);
-    if(numMonths <= 1){
-        flex = "0 0 11%";
-        fontsize = "30px";
-    } else if(numMonths <= 3){
-        flex = "0 0 6%";
-        fontsize = "20px";
-    } else if(numMonths <= 6){
-        flex = "0 0 4%";
-        fontsize = "15px";
-    } else if(numMonths <= 9){
-        flex = "0 0 3%";
-        fontsize = "13px";
-    } else{
-        flex = "0 0 2.5%";
-        fontsize = "12px";
+    let fontsize = "30px";
+    console.log(numMonths + " = numMonths being passed into creatDays function.");
+   
+    //changes what days look like depending on if the user selected it in misc settings
+    if(document.getElementById("boxSizing").checked){
+        if(numMonths <= 1){
+            flex = "0 0 12%";
+            fontsize = "30px";
+        } else if(numMonths <= 3){
+            flex = "0 0 6%";
+            fontsize = "20px";
+        } else if(numMonths <= 6){
+            flex = "0 0 4%";
+            fontsize = "15px";
+        } else if(numMonths <= 9){
+            flex = "0 0 3%";
+            fontsize = "13px";
+        } else{
+            flex = "0 0 2.5%";
+            fontsize = "12px";
+        }
     }
 
     for(let i = 0; i < numMonths; i++) {
-        const currentMonth = (monthPassed + i) % 12;
-        const currentYear = selectedYear + Math.floor((monthPassed + i) / 12); // Adjust the year based on overflow
+        let indexMonth = (monthPassed + i) % 12;
+        const indexYear = selectedYear + Math.floor((monthPassed + i) / 12); // Adjust the year based on overflow
 
-        for(let day = 0; day < daysInMonth(currentMonth, currentYear); day++) {
+        for(let day = 0; day < daysInMonth(indexMonth, indexYear); day++) {
             const dayDiv = document.createElement("div");
-            dayDiv.id = (day + 1) + "/" + (currentMonth + 1) + "/" + currentYear; // d/m/y
+            dayDiv.id = (day + 1) + "/" + (indexMonth + 1) + "/" + indexYear; // d/m/y
             dayDiv.className = 'day';
             dayDiv.style.flex = flex;
+            dayDiv.style.borderColor = borderColor;
             
-            if(dateFormat == "dmy"){
-                dayDiv.textContent = dayDiv.id;
+            //value dependent on if it's in the future or not. CURRENTLY ISN'T WORKING??
+            if(selectedYear > currentYear ){
+                dayDiv.dataset.value = "future";
+                dayDiv.style.backgroundcolor = futureColor;
+            }else if(selectedYear == currentYear && selectedMonth > currentMonth){
+                dayDiv.dataset.value = "future";
+                dayDiv.style.backgroundcolor = futureColor;
+            }else if(selectedYear == currentYear && selectedMonth == currentMonth && day > currentDay){
+                dayDiv.dataset.value = "future";
+                dayDiv.style.backgroundcolor = futureColor;
             }else{
-                let test = dayDiv.id.split("/");
-                dayDiv.textContent = test[1] + "/" + test[0] + "/" + test[2];
+                dayDiv.dataset.value = "100%";
+                dayDiv.style.backgroundcolor = hundredColor;
             }
-            
+            console.log(dayDiv.dataset.value);
+            //if user wants the date on the day boxes, add text content
+            if(document.getElementById("showDate").checked){
+                //make text content the correct date formatting based on user selection
+                if(dateFormat == "dmy"){
+                    dayDiv.textContent = dayDiv.id;
+                }else{
+                    let test = dayDiv.id.split("/");
+                    dayDiv.textContent = test[1] + "/" + test[0] + "/" + test[2];
+                }
+            }
             dayDiv.style.fontSize = fontsize;
             dayContainer.appendChild(dayDiv);
+        }
+
+        //create a line that splits the months up. 
+        //if statement makes it so it doesnt add it at the very end and if the user wants it or not.
+        if(i != numMonths-1 && document.getElementById("HR").checked){
+            var monthSplitLine = document.createElement("hr"); 
+            monthSplitLine.style.flex = "0 0 98%";
+            monthSplitLine.style.margin = "0 auto";
+            monthSplitLine.style.backgroundColor = "black";
+            dayContainer.appendChild(monthSplitLine);
         }
     }
 }
 
+//change days onscreen
 function dayChanger(numMonths, nextOrPrev) {
     // Calculate the new month index based on direction
+    console.log(numMonths + "numMonths in daychanger")
     if (nextOrPrev === "next") {
-        selectedMonth += numMonths;
+        selectedMonth += Number(numMonths); //for some reason it was doing a string addition, needed to typecast.
     } else if (nextOrPrev === "prev") {
         selectedMonth -= numMonths;
+    } else if(nextOrPrev === "ytd"){
+        prevSelectedMonth = selectedMonth;
+        selectedMonth -= numMonths;
+        numMonths++;
     }
+    console.log(numMonths + " numMonths in daychanger after")
 
     // Adjust the year based on overflow or underflow
     while (selectedMonth >= 12) {
@@ -92,22 +156,37 @@ function dayChanger(numMonths, nextOrPrev) {
         selectedMonth += 12; // Wrap around to 0-11
     }
 
-    // Log for debugging
     console.log(`Selected Month: ${selectedMonth + 1}, Selected Year: ${selectedYear}`); // Output for verification
 
     // Clear the current days and create new ones
     removeElementsByClass("dayContainer");
+    //create days based off of these arguments.
+    //selected month is what the starting month is, selected year is so we can tell what year we are in, 
+    //and number of months is how many months we need to create
     createDays(selectedMonth, selectedYear, numMonths);
+    if(nextOrPrev === "ytd"){
+        selectedMonth = prevSelectedMonth;
+    }
     addDayClickListeners();
 }
 
 //next arrow on the header
 document.getElementById("next").onclick = function() {
+    //account for if ytd is selected. if it is make the year view if we change it
+    if(ytdSelected === true){
+        view = 12;
+    }
+
     dayChanger(view, "next");
 }
 
 //prev arrow on the header
 document.getElementById("prev").onclick = function() {
+    //account for if ytd is selected. if it is make the year view if we change it
+    if(ytdSelected === true){
+        view = 12;
+    }
+
     dayChanger(view, "prev");
 }
 
@@ -129,6 +208,13 @@ function addDayClickListeners() {
     for (let i = 0; i < days.length; i++) {
         const dayPopUp = document.getElementById("dayPopUp");
         days[i].onclick = function() {
+            
+            //hide settings popup if someone clicks on a day.
+            if(document.getElementById('settingsPopUp').style.visibility === "visible"){
+                document.getElementById('settingsPopUp').style.visibility = "hidden";
+            }
+
+            //show or hide day popup depending on if it's visible and if it's the same day previously clicked or not 
             if (dayPopUp.style.visibility === 'visible' && prevID == days[i].id) {
                 removeElementsByClass("dayPopUpText");
                 dayPopUp.style.visibility = 'hidden';
@@ -137,10 +223,12 @@ function addDayClickListeners() {
                 dayPopUp.style.visibility = 'visible';
                 removeElementsByClass("dayPopUpText"); 
 
+                //this is what is actually on the daypopup
                 const date = document.createElement("p");
                 date.id = days[i].id + "date";
                 date.className = "dayPopUpText";
 
+                //format the date based off of what is currently selected.
                 if (dateFormat === "dmy") {
                     date.textContent = days[i].id;
                 }
@@ -149,7 +237,10 @@ function addDayClickListeners() {
                     date.textContent = parts[1] + "/" + parts[0] + "/" + parts[2];
                 }
                 
+                //append date to the popup.
                 dayPopUp.appendChild(date); 
+
+                //save id of currently selected day. This is used to close the day if it's clicked again.
                 prevID = days[i].id;
             }
         };
@@ -180,6 +271,9 @@ function toggleDisplayByClass(className, displayType = 'block') {
 
 // When the gear icon is clicked, show/hide settings popup
 document.getElementById('settings').onclick = function() {
+    if(document.getElementById('dayPopUp').style.visibility === "visible"){
+        document.getElementById('dayPopUp').style.visibility = "hidden";
+    }
     toggleVisibilityById('settingsPopUp');
 }
 
@@ -198,30 +292,42 @@ document.getElementById('changeView').onclick = function() {
     toggleDisplayByClass('viewSettings', 'block'); // Use 'block' as display type for this dropdown
 }
 
+// When 'miscSettings' is clicked, show/hide the view settings dropdown
+document.getElementById('miscSettings').onclick = function() {
+    toggleDisplayByClass('miscSettings', 'block'); // Use 'block' as display type for this dropdown
+}
+
 //check what view is selected
 document.getElementById("1month").onclick = function() {
+    ytdSelected = false;
     view = 1;
     dayChanger(view, "");
 }
 document.getElementById("3month").onclick = function() {
+    ytdSelected = false;
     view = 3;
     dayChanger(view, "");
 }
 document.getElementById("6month").onclick = function() {
+    ytdSelected = false;
     view = 6;
     dayChanger(view, "");
 }
 document.getElementById("9month").onclick = function() {
+    ytdSelected = false;
     view = 9;
     dayChanger(view, "");
 }
 document.getElementById("year").onclick = function() {
+    ytdSelected = false;
     view = 12;
     dayChanger(view, "");
 }
 document.getElementById("ytd").onclick = function() {
-    view = 12 - currentMonth;
-    dayChanger(view, "");
+    ytdSelected = true;
+    selectedMonth = currentMonth;
+    view = currentMonth;
+    dayChanger(view, "ytd");
 }
 
 // When 'change format' is clicked, show/hide the format settings dropdown
@@ -232,7 +338,67 @@ document.getElementById('changeFormat').onclick = function() {
 //check which format is selected
 document.getElementById("dmy").onclick = function() {
     dateFormat = "dmy";
+    dayChanger(view, "");
 }
 document.getElementById("mdy").onclick = function() {
     dateFormat = "mdy";
+    dayChanger(view, "");
 }
+
+//refresh when hr is clicked.
+document.getElementById("HR").onclick = function() {
+    dayChanger(view, "");
+}
+
+//refresh when boxSizing is clicked.
+document.getElementById("boxSizing").onclick = function() {
+    dayChanger(view, "");
+}
+
+//refresh when showDate is clicked.
+document.getElementById("showDate").onclick = function() {
+    dayChanger(view, "");
+}
+
+//===================================================COLOR CUSTOMIZATION===================================================
+document.getElementById('backgroundSubmit').addEventListener('click', function() {
+    const color = document.getElementById('backgroundColor').value;
+    document.body.style.backgroundColor = color;
+});
+
+//uses global variable for borderColor
+document.getElementById('borderSubmit').addEventListener('click', function() {
+    borderColor = document.getElementById('borderColor').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('futureBoxSubmit').addEventListener('click', function() {
+    futureColor = document.getElementById('futureBoxColor').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('box100Submit').addEventListener('click', function() {
+    hundredColor = document.getElementById('box100Color').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('box75Submit').addEventListener('click', function() {
+    seventyFiveColor = document.getElementById('box75Color').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('box50Submit').addEventListener('click', function() {
+    fiftyColor = document.getElementById('box50Color').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('box25Submit').addEventListener('click', function() {
+    twentyFiveColor = document.getElementById('box25Color').value;
+    dayChanger(view, "");
+});
+
+document.getElementById('box0Submit').addEventListener('click', function() {
+    zeroColor = document.getElementById('box0Color').value;
+    console.log(zeroColor);
+    dayChanger(view, "");
+});
